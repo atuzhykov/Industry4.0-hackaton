1bad496ca1a27262daac7dce1f0f1d3b15e42091
atuzhykov <andrewtuzhykov@gmail.com>
2018-11-24 17:12:54 +0200

    changed claster visualization to 3D

--- chemical_pipeline_KMeans.py
+++ chemical_pipeline_KMeans.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans  
 from data_fixer import data_fixer
@@ -12,19 +13,15 @@ df = pd.read_csv(input_file, delimiter = ";", usecols=[3,4,5,6])
 
 valid_data = data_fixer(df)
 
-# model = TSNE(learning_rate=100)
-# transformed = model.fit_transform(valid_data)
-# x_axis = transformed[:, 0]
-# y_axis = transformed[:, 1]
 
-# plt.scatter(x_axis, y_axis)
-# plt.show()
+fig = plt.figure()
+ax = plt.axes(projection='3d')
 
 
 X = np.array(valid_data)
 kmeans = KMeans(n_clusters=4)  
 kmeans.fit(X) 
-plt.scatter(X[:,0],X[:,1], c=kmeans.labels_, cmap='rainbow') 
+ax.scatter(X[:,0],X[:,1],X[:,2], c=kmeans.labels_, cmap='rainbow') 
 plt.show() 
 
 
---
+++ chemical_pipeline_tSNE.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans  
+from data_fixer import data_fixer
+
+
+
+input_file = "chemical_sensors_data.csv"
+df = pd.read_csv(input_file, delimiter = ";", usecols=[3,4,5,6])
+
+valid_data = data_fixer(df)
+
+model = TSNE(learning_rate=100)
+transformed = model.fit_transform(valid_data)
+x_axis = transformed[:, 0]
+y_axis = transformed[:, 1]
+
+plt.scatter(x_axis, y_axis)
+plt.show()
+
+
+
+
+
